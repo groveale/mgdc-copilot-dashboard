156c282fbc86ea0e9df0400b145436789d44c47b
Alex Grover <alexgrover@microsoft.com>
2023-10-24 09:51:52 +0100

    Added cert and sample dashboard

---
+++ CreateSelfSignedCert.ps1
@@ -0,0 +1,3 @@
+$certname = "copilot-dashboard"    ## Replace {certificateName}
+$cert = New-SelfSignedCertificate -Subject "CN=$certname" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
+Export-Certificate -Cert $cert -FilePath "$certname.cer"   ## Specify your preferr
